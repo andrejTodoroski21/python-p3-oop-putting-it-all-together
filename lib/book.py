1eccdf3afd778abe2fd7abd51640390236cafd31
Andrej <andrejtodoroski@yahoo.com>
2024-04-23 13:56:37 -0400

    done

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,26 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
+    def __init__(self, title:str, page_count:int):
+        self.title = title
+        self._page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
+
+    @property
+    def page_count(self):
+        return self._page_count
+
+
+    @page_count.setter
+    def page_count(self, value:int):
+        if type(value) != int:
+            print("page_count must be an integer")
+        else:
+            self.page_count = value
+
+
+
     
         
--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,25 @@
 #!/usr/bin/env python3
 
 class Shoe:
-    pass
+    def __init__(self, brand:str, size:int):
+        self.brand  = brand
+        self._size = size
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+    @property
+    def size(self):
+        return self._size
+
+
+    @size.setter
+    def size(self, value:int):
+        if type(value) != int:
+            print("size must be an integer")
+        else:
+            self.size = value
+
+
+        
